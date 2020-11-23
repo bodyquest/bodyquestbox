73b6808b5d04458d6455353c05af9399590e077f
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-11-23 01:11:08 +0100

    Implement sorting

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ICategory } from '../shared/models/category';
-import { IProduct } from '../shared/models/pagination';
+import { IPagination, IProduct } from '../shared/models/pagination';
 import { ShopService } from './shop.service';
 
 @Component({
@@ -9,8 +9,16 @@ import { ShopService } from './shop.service';
   styleUrls: ['./shop.component.scss']
 })
 export class ShopComponent implements OnInit {
-  products!: IProduct[];
+  products: IProduct[] = [];
   categories!: ICategory[];
+
+  categoryIdSelected = 0;
+  sortSelected = 'name';
+  sortOptions = [
+    {name: 'Alphabetical', value: 'name'},
+    {name: 'Price: Low to High', value: 'priceAsc'},
+    {name: 'Price: High to Low', value: 'priceDesc'}
+  ]
 
   constructor(private shopService: ShopService) { }
 
@@ -20,9 +28,14 @@ export class ShopComponent implements OnInit {
   }
 
   getProducts() {
-    this.shopService.getProducts()
-      .subscribe(response => {
-        this.products = response.data;
+    this.shopService.getProducts(this.sortSelected)
+      .subscribe((response: IPagination | null) => {
+        if (response) {
+          this.products = response.data;
+        }
+        else{
+         
+        }
       }, error => {
         console.log(error);
       });
@@ -35,5 +48,11 @@ export class ShopComponent implements OnInit {
       }, error => {
         console.log(error);
       });
+  }
+
+  onSortSelected(event: Event) {
+    let element = event.target as HTMLSelectElement;
+    this.sortSelected = element.value;
+    this.getProducts();
   }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { IPagination } from '../shared/models/pagination';
 import { ICategory } from '../shared/models/category';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,20 @@ export class ShopService {
 
   constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get<IPagination>(this.baseUrl + 'products');
+  getProducts(sort?: string) {
+    let params = new HttpParams();
+
+    if (sort) {
+      params = params.append('sort', sort);
+    }
+
+    return this.http.get<IPagination>
+      (this.baseUrl + 'products', {observe: 'response', params})
+      .pipe(
+        map(response => {
+          return response.body;
+        })
+      );
   }
 
   getCategories() {
