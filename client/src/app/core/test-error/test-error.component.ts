0e5669f8fbe05a71e8fbd289ce17e4ab8c08defb
Darin Rusev <46286814+bodyquest@users.noreply.github.com>
2020-11-24 17:35:24 +0100

    Implement HttpInterceptr to catch errors / Add Toastr Notifications / Handle validation errors

--- client/src/app/core/test-error/test-error.component.ts
+++ client/src/app/core/test-error/test-error.component.ts
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 })
 export class TestErrorComponent implements OnInit {
   baseUrl = environment.apiUrl;
+  validationErrors: any;
 
   constructor(private http: HttpClient) { }
 
@@ -49,6 +50,7 @@ export class TestErrorComponent implements OnInit {
         console.log(response);
       }, error => {
         console.log(error);
+        this.validationErrors = error.errors;
       });
   }
 }
